d1be78e2adc5f815a66fb0f89dda9b1e2dfec2e3
Alinwachukwu Michael <saintmichaell42@gmail.com>
2023-01-31 18:34:17 +0100

    added productPage

--- src/pages/Shopnow/ShopnowStyle.js
+++ src/pages/Shopnow/ShopnowStyle.js
@@ -1,14 +1,40 @@
 import styled from "@emotion/styled";
 
 export const Container = styled.div`
-  margin: auto;
-  width: 100%;
-  max-width: 900px;
-  position: relative;
-  border-bottom: 1px solid #e8e7ec;
+margin: auto;
+width: 100%;
+max-width: 900px;
+border-bottom: 1px solid #e8e7ec;
 
-  @media screen and (max-width: 815px) {
+& > * {
+    align-items: center;
+}
+.products-heading {
+    text-align: center;
+}
+.products-container{
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    gap: 15px;
+    margin-top: 20px;
+    width: 100%;
   }
-  @media screen and (max-width: 320px) {
-  }
+
+
+
+
+@media screen and (max-width: 900px) {
+    width: 100%;
+}
+@media screen and (max-width: 750px) {
+        // width: 80%;
+}
+@media screen and (max-width: 600px) {
+    width: 100%;
+    
+}
+@media screen and (max-width: 320px) {
+    width: 100%; 
+}
 `;
