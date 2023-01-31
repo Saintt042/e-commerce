8b8ae5b678b5bb072c5245a37de664471e5ca3fe
Alinwachukwu Michael <saintmichaell42@gmail.com>
2023-01-31 21:04:17 +0100

    added contactPage

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -4,6 +4,10 @@ import Navbar from "../../components/Navbar/Navbar";
 import AppContext from "../../components/Context/Appcontext";
 import Cart from "../../components/Cart/Cart";
 import Footer from "../../components/Footer/Footer";
+import { Button, TextField } from "@mui/material";
+import SendIcon from '@mui/icons-material/Send';
+import FormControl from "@mui/material/FormControl";
+// import bg from "../../bgcontact.jpeg";
 
 const Contact = () => {
   const { showCart } = useContext(AppContext);
@@ -12,6 +16,50 @@ const Contact = () => {
       <div>
         {showCart && <Cart />}
         <Navbar />
+        {/* <img src={bg} alt="Background" /> */}
+        <form
+        noValidate
+        autoComplete="off"
+        onSubmit=''
+        action="">
+        <FormControl className="formcontrol" >
+          <div className="names">
+            <TextField
+              className="textfield"
+              helperText="Please enter your first name"
+              id="demo-helper-text-aligned"
+              label="First Name"
+            />
+            <TextField
+              className="textfields"
+              helperText="Please enter your last name"
+              id="demo-helper-text-aligned"
+              label="Last Name"
+            />
+          </div>
+          <TextField
+            className="textfield"
+            helperText="Please enter your email"
+            id="demo-helper-text-aligned"
+            label="Email"
+          />
+          <TextField
+            id="outlined-multiline-static"
+            label="We listen"
+            multiline
+            rows={4}
+            defaultValue="Tell us..."
+            helperText="By Clicking send, you agree to subscribe with us"
+          />
+          <Button 
+          type="submit"
+          variant="contained" 
+          endIcon={<SendIcon />}>
+        Send
+      </Button>
+        </FormControl>
+        </form>
+
         <Footer />
       </div>
     </Container>
--- src/pages/Contact/ContactStyles.js
+++ src/pages/Contact/ContactStyles.js
@@ -1,15 +1,39 @@
 import styled from "@emotion/styled";
 
+
 export const Container = styled.div`
   margin: auto;
   width: 100%;
   max-width: 900px;
   position: relative;
   border-bottom: 1px solid #e8e7ec;
-
+  background: #e8e7ec;
+ 
+//   img {
+//     width: 100%;
+//     max-width: 1360px;
+//     max-height: 550px;
+// height: 550px;
+// }
+.formcontrol {
+    padding: 40px 15%;
+    margin: 40px;
+    gap: 50px;   
+}
+ .textfields {
+    margin-left: 20px;
+ }
+   
 
   @media screen and (max-width: 815px) {
+    
+ .textfields {
+    margin-left: 0;
+ }
   }
   @media screen and (max-width: 320px) {
+    .textfields {
+        margin-left: 0;
+     }
   }
 `;
